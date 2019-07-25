ac1117d447b7c20776a40c4bad19ff822a5f590e
Ryan <chaosmos1383@gmail.com>
2019-07-25 18:40:58 +0900

    Updates

---
+++ precache-manifest.2932748f9652d631529cc453d1e5635e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391e9b411bb1706a1763deb9afea3a1b",
+    "url": "/MovieRank/index.html"
+  },
+  {
+    "revision": "6afd49a57ee5fb720d32",
+    "url": "/MovieRank/static/css/main.a1fb7018.chunk.css"
+  },
+  {
+    "revision": "3c0314539bbaab0b4cbf",
+    "url": "/MovieRank/static/js/2.e210c8ae.chunk.js"
+  },
+  {
+    "revision": "6afd49a57ee5fb720d32",
+    "url": "/MovieRank/static/js/main.0591a66f.chunk.js"
+  },
+  {
+    "revision": "221b558202da86baff51",
+    "url": "/MovieRank/static/js/runtime~main.bf2f4ff1.js"
+  },
+  {
+    "revision": "3910ec0e68513e9be74b7cf89f230812",
+    "url": "/MovieRank/static/media/avatar.3910ec0e.png"
+  },
+  {
+    "revision": "76eb92724f0ca7837393585dda9d485e",
+    "url": "/MovieRank/static/media/star.76eb9272.png"
+  }
+]);
